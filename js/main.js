d0d07624e64385b5506b68edd28de9e25924943e
coltonsaywhatt <coltonsaywhatt@gmail.com>
2022-04-04 19:46:53 -0400

    added middle master deck with shuffle function along with button hide on hit and stand

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
 /*----- constants -----*/
 const suits = ["Clubs", "Diamonds", "Hearts", "Spades"];
 const faces = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const masterDeck = buildMasterDeck();
 
 
 /*----- app's state (variables) -----*/
 let deck = [];
 let playerCards = [];
 let dealerCards = [];
+let shuffledDeck = [];
 
 /*----- cached element references -----*/
-let newGameButton = document.querySelector('#new-game-button');
+let dealButton = document.querySelector('#deal-button');
 let hitButton = document.querySelector('#hit-button');
 let stayButton = document.querySelector('#stay-button');
+let playButton = document.querySelector('#playAgain-button');
 
 /*----- event listeners -----*/
 hitButton.addEventListener('click', function() {
@@ -28,22 +31,48 @@ newGameButton.addEventListener('click', function() {
 
 /*----- functions -----*/
 
-hitButton.style.display = 'none';
-stayButton.style.display = 'none';
+startGame();
 
 function startGame() {
-
+  hitButton.style.display = 'none';
+  stayButton.style.display = 'none';
+  playButton.style.display = 'none';
+  deck = getNewShuffledDeck();
+  render();
 }
 
-function card() {
-
+function render() {
+  console.log(deck)
 }
 
-function shuffleDeck() {
 
+function buildMasterDeck() {
+    const deck = [];
+    suits.forEach(function(suit) {
+        faces.forEach(function(rank) {
+            deck.push({          
+                face: `${suit}${rank}`,
+                value: Number(rank) || (rank === 'A' ? 11 : 10)
+            });
+        });
+    });
+    return deck;
 }
 
-shuffleDeck();
+function getNewShuffledDeck() {
+    // Create a copy of the masterDeck (leave masterDeck untouched!)
+    const tempDeck = [...masterDeck];
+    const newShuffledDeck = [];
+    while (tempDeck.length) {
+      // Get a random index for a card still in the tempDeck
+      const rndIdx = Math.floor(Math.random() * tempDeck.length);
+      // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
+      newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+    }
+    return newShuffledDeck;
+  }
+
+
 
 function playerHand() {
     
