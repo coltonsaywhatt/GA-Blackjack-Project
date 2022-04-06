c93fc10184e8aa8234b12c42c2f5e906894c778e
coltonsaywhatt <coltonsaywhatt@gmail.com>
2022-04-06 18:39:41 -0400

    fixed cards from duplicating and hid one card from the dealer hand

--- js/main.js
+++ js/main.js
@@ -11,11 +11,10 @@ let playerWon = false;
 let playerCards = [];
 let dealerCards = [];
 
-let dealerScore = 0;
-let playerScore = 0;
+// let dTotal;
+// let pTotal;
 
 let shuffledDeck;
-let value;
 
 /*----- cached element references -----*/
 let dealButton = document.querySelector('#deal-button');
@@ -25,24 +24,21 @@ let playButton = document.querySelector('#playAgain-button');
 let playerCardContainer = document.getElementById('player-cards')
 let dealerCardContainer = document.getElementById('dealer-cards')
 
+
+// const playerTotalEl = document.getElementById('player-total');
+// const dealerTotalEl = document.getElementById('dealer-total');
+
 /*----- event listeners -----*/
 
 // DO NOT TOUCH!!!!!!
 
 hitButton.addEventListener('click', hit);
 
-
 stayButton.addEventListener('click', stay);
-  // gameOver = true;
 
 dealButton.addEventListener('click', dealCards);
 
-
 playButton.addEventListener('click', playAgain)
-  // gamesStarted = true;
-  // gameOver = false;
-  // playerWon = false;
-
 
 /*----- functions -----*/
 
@@ -53,6 +49,7 @@ function init() {
   stayButton.style.display = 'none';
   playButton.style.display = 'none';
   shuffledDeck = getNewShuffledDeck();
+  // pTotal = dTotal = 0;
   render();
 }
 
@@ -63,64 +60,26 @@ function render() {
 
 
 function buildMasterDeck() {
-    const deck = [];
-    suits.forEach(function(suit) {
-        faces.forEach(function(rank) {
-            deck.push({          
-                face: `${suit}${rank}`,
-                value: Number(rank) || (rank === 'A' ? 11 : 10)
-            });
-        });
+  const deck = [];
+  suits.forEach(function(suit) {
+    faces.forEach(function(rank) {
+      deck.push({          
+        face: `${suit}${rank}`,
+        value: Number(rank) || (rank === 'A' ? 11 : 10)
+      });
     });
-    return deck;
+  });
+  return deck;
 }
 
 function getNewShuffledDeck() {
-    const tempDeck = [...masterDeck];
-    const newShuffledDeck = [];
-    while (tempDeck.length) {
-      const rndIdx = Math.floor(Math.random() * tempDeck.length);
-      newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
-    }
-    return newShuffledDeck;
+  const tempDeck = [...masterDeck];
+  const newShuffledDeck = [];
+  while (tempDeck.length) {
+    const rndIdx = Math.floor(Math.random() * tempDeck.length);
+    newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
   }
-
-
-// player.Cards[0].value + playerCards[1].value
-// creating a new div element
-// we are creating a variable that is storing our class name to ref face value of our object which will be added to our div
-// creating a card variable that creates a div that is added to our player container
-// 
-function renderPlayerHand() {
-  playerCards.forEach(function(playerCard) {
-    const cardEl = document.createElement('div');
-    // giving a class name 
-    cardEl.className = `card ${playerCard.face}`;
-    playerCardContainer.appendChild(cardEl);
-    //console.log(`"playerCard" ${playerCard.face}`)
-    
-    // $('#player-cards').appendChild('<div class="player-value">' + dealerCards + '</div');
-
-  });
-    
-}
-
-function renderDealerHand() {
-  dealerCards.forEach(function(dealerCard) {
-    const CardEl = document.createElement('div');
-    cardEl.className = `card ${dealerCard.face}`;
-    dealerCardContainer.appendChild(cardEl);    
-  })
-
-}
-
-function getNextCard() {
-  return shuffledDeck.shift();
-}
-
-function hit() {
-  playerCards.push(getNextCard());
-  render();
+  return newShuffledDeck;
 }
 
 function dealCards() {
@@ -129,72 +88,84 @@ function dealCards() {
   playButton.style.display = 'none';
   dealButton.style.display = 'none';
 
+  gameStarted = true;
+  gameOver = false;
+  playerWon = false
+
   dealerCards = [getNextCard(), getNextCard()];
   playerCards = [getNextCard(), getNextCard()];
 
+  // dTotal = getHandTotal(dealerCards);
+  // pTotal = getHandTotal(playerCards);
+
   render();
-  // checkBj();
+}
+
+// player.Cards[0].value + playerCards[1].value
+// creating a new div element
+// we are creating a variable that is storing our class name to ref face value of our object which will be added to our div
+// creating a card variable that creates a div that is added to our player container
+
+function renderPlayerHand() {
+  playerCards.forEach(function(playerCard) {
+    const cardEl = document.createElement('div');
+    cardEl.className = `card ${playerCard.face}`;
+    playerCardContainer.appendChild(cardEl); 
+  })
+  // playerTotalEl.innerHTML = pTotal;    
+}
+
+function renderDealerHand() {
+  dealerCards.forEach(function(dealerCard, idx) {
+    if(idx === 1) {
+      const cardEl = document.createElement('div');
+      cardEl.className = `card back`;
+      dealerCardContainer.appendChild(cardEl);
+    } else {
+      const cardEl = document.createElement('div');
+    cardEl.className = `card ${dealerCard.face}`;
+    dealerCardContainer.appendChild(cardEl); 
+    }       
+  })
+  // dealerTotalEl.innerHTML = outcome ? dTotal : '??';
+}
+
+function clearHand() {
+  Array.from(playerCardContainer.children).forEach(function(card) {
+    playerCardContainer.removeChild(card)
+  })
+
+  Array.from(dealerCardContainer.children).forEach(function(card) {
+    dealerCardContainer.removeChild(card)
+  })
+}
+
+function getNextCard() {
+  return shuffledDeck.shift();
+}
+
+function hit() {
+  clearHand();
+  playerCards.push(getNextCard());
+  // checkForBlackJack();
+  render();
 }
 
 function stay() {
-  // checkBj();
+  // gameOver = true;
+  // checkForBlackJack();
+
+}
+
+function playAgain() {
+
 }
 
 
 
-// function getCardValue(card) {
-//   switch (card.rank) {
-//     case 'A':
-//       return 1;
-//     case 2:
-//       return 2;
-//     case 3:
-//       return 3;
-//     case 4:
-//       return 4;
-//     case 5:
-//       return 5;
-//     case 6:
-//       return 6;
-//     case 7:
-//       return 7;
-//     case 8:
-//       return 8;
-//     case 9:
-//       return 9;
-//     default:
-//       return 10;
-//   }
-// }
-
-// function getScore(cardArray) {
-//   let score = 0;
-//   let hasAce = false;
-//   for (let i = 0; i < cardArray.length; i++) {
-//     let card = cardArray[i];
-//     score += getCardValue(card);
-//     if (card === "Ace") {
-//       hasAce = true;
-//     }
-//   }
-//   if (hasAce && score + 10 <= 21) {
-//     return score + 10;
-//   }
-//   return score;
-// }
-
-// function updateScores() {
-//   let dealerScore = getScore(dealerCards);
-//   let playerScore = getScore(playerCards);
-// }
 
 
-
-
-
-
-
-// function checkBj() {
+// function checkForBlackJack() {
 //   updateScores();
 //     if(gameOver) {
 //         while(dealerScore < playerScore && playerScore <= 21 && dealerScore <= 21) {
@@ -216,16 +187,3 @@ function stay() {
 //         }
 //     }
 // }
-
-function playAgain() {
-
-}
-
-// function showPlayerValue() {
-//   $('.player-cards').appendChild('<div class="player-value">' + playerCards + '</div');
-// }
-
-// function showDealerValue() {
-//   $('.dealer-cards').appendChild('<div class="computer-value">' + dealerCards + '</div');
-// }
-
